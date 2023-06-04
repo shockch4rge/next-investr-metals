a8905ffe0c1e7f0d556a96c0d61e29c75a915d10
Favian <favouriteteo@gmail.com>
2023-06-05 04:12:24 +0800

    feat: add trading timeframe

--- src/contexts/TradingPreferences.tsx
+++ src/contexts/TradingPreferences.tsx
@@ -3,22 +3,27 @@
 import type { Dispatch, PropsWithChildren, SetStateAction } from "react";
 import { createContext, useContext, useEffect, useState } from "react";
 
-export type MetalType = "gold" | "palladium" | "platinum" | "silver";
-export type CurrencyType = "eur" | "sgd" | "usd";
+export type MetalType = "XAG" | "XAU" | "XPD" | "XPT";
+export type CurrencyType = "EUR" | "SGD" | "USD";
+export type TradingTimeframe = "1d" | "1m" | "1w" | "1y" | "6m";
 
 interface State {
     metalType: MetalType;
     currency: CurrencyType;
+    timeframe: TradingTimeframe;
     setMetalType: (value: MetalType) => void;
     setCurrency: (value: CurrencyType) => void;
+    setTimeframe: (value: TradingTimeframe) => void;
 }
 
 const initialState = {
-    metalType: "gold",
-    currency: "sgd",
-    // dummy function
+    metalType: "XAU",
+    currency: "SGD",
+    timeframe: "1w",
+    // dummy functions
     setMetalType: () => {},
     setCurrency: () => {},
+    setTimeframe: () => {},
 } satisfies State;
 
 export const TradingPreferencesContext = createContext<State>(initialState);
@@ -26,8 +31,9 @@ export const TradingPreferencesContext = createContext<State>(initialState);
 export const TradingPreferencesProvider: React.FC<PropsWithChildren> = ({ children }) => {
     const [currency, setCurrency] = useState<CurrencyType>(initialState.currency);
     const [metalType, setMetalType] = useState<MetalType>(initialState.metalType);
+    const [timeframe, setTimeframe] = useState<TradingTimeframe>(initialState.timeframe);
 
-    return <TradingPreferencesContext.Provider value={{ currency, setCurrency, setMetalType, metalType }}>
+    return <TradingPreferencesContext.Provider value={{ currency, setCurrency, timeframe, setTimeframe, setMetalType, metalType }}>
         {children}
     </TradingPreferencesContext.Provider>;
 };
