351a8a1d15ba1ce6d32d4f4725ca13162a23a6fb
Favian <favouriteteo@gmail.com>
2023-06-05 04:08:29 +0800

    deps: updated

--- src/components/dashboard/TradingChart.tsx
+++ src/components/dashboard/TradingChart.tsx
@@ -1,80 +1,118 @@
 "use client";
 
-import type { CurrencyType } from "@/contexts/TradingPreferences";
+import { MetalPriceResponseSchema, type MetalPriceResponse, MetalPrice } from "@/apis/metals";
+import type { CurrencyType, TradingTimeframe } from "@/contexts/TradingPreferences";
 import { useTradingPreferences } from "@/contexts/TradingPreferences";
 import { Card, AreaChart, TabList, Tab, Text, Metric, Subtitle, SelectBox, SelectBoxItem } from "@tremor/react";
-import { useEffect, useState } from "react";
+import { DateTime } from "luxon";
+import { useCallback, useEffect, useState } from "react";
+
+async function getMetalPrices({ timeframe, metal }: { metal: "XAG" | "XAU" | "XPD" | "XPT"; timeframe: TradingTimeframe }) {
+    let startDate = DateTime.now().minus({ days: 2 })
+        .toISODate();
+    const endDate = DateTime.now().minus({ days: 1 })
+        .toISODate();
+
+    if (timeframe === "1y") {
+        startDate = DateTime
+            .now()
+            .minus({ years: 1 })
+            .toISODate()!;
+    }
+
+    if (timeframe === "6m") {
+        startDate = DateTime
+            .now()
+            .minus({ months: 6 })
+            .toISODate()!;
+    }
+    
+    if (timeframe === "1m") {
+        startDate = DateTime
+            .now()
+            .minus({ months: 1 })
+            .toISODate()!;
+    }
+    
+    if (timeframe === "1w") {
+        startDate = DateTime
+            .now()
+            .minus({ weeks: 1 })
+            .toISODate();
+    }
+    
+    const url = `https://api.metalpriceapi.com/v1/timeframe?start_date=${startDate}&end_date=${endDate}&base=${metal}&currencies=USD`;
+    const res = await fetch(url, {
+        headers: {
+            "X-API-KEY": process.env.NEXT_PUBLIC_METALPRICE_API_KEY!,
+            "Content-Type": "application/json",
+        },
+    });
+    const data = await res.json();
+
+    return MetalPriceResponseSchema.parseAsync(data);
+}
+
+async function getConvertedCurrency({ currency, rates }: { currency: CurrencyType; rates: number[] }) {
+    const res = await Promise.all(rates.map(r => {
+        const url = `https://api.exchangerate.host/convert?from=USD&to=${currency}&amount=${r}&places=2`;
+        return fetch(url);
+    }));
+    const data = await Promise.all(res.map(r => r.json()));
+    return data as any[];
+}
+
 
 export const TradingChart: React.FC = () => {
-    const actualPrice = 1977.74;
+    const [localePrices, setLocalePrices] = useState<Array<{ date: string; Price: number }>>([]); 
+    const { metalType, currency, setCurrency, timeframe, setTimeframe } = useTradingPreferences();    
 
-    const [price, setPrice] = useState(1977.74);
-    const { metalType, currency, setCurrency } = useTradingPreferences();
-
-    const chartdata = [
-        {
-            date: "Jan 22",
-            "Trading Volume": 2890.23,
-        },
-        {
-            date: "Feb 22",
-            "Trading Volume": 2756.42,
-        },
-        {
-            date: "Mar 22",
-            "Trading Volume": 3322.87,
-        },
-        {
-            date: "Apr 22",
-            "Trading Volume": 3470.70,
-        },
-        {
-            date: "May 22",
-            "Trading Volume": 3475.15,
-        },
-        {
-            date: "Jun 22",
-            "Trading Volume": 3129.52,
-        },
-    ];
-
-    useEffect(() => {
-        fetch(`https://api.exchangerate.host/convert?from=USD&to=${currency.toUpperCase()}&amount=${actualPrice}&places=2`)
-            .then(res => res.json())
-            .then(data => setPrice(data.result));
-    }, [currency]);
-
-    const formatPrice = (price: number) => {
-        // display either dollar or euro depending on the currency type
-        return currency === "eur" ? `€${Intl.NumberFormat("de").format(price)}` : `$${Intl.NumberFormat("us").format(price)}`;
+    const matchCurrencyType = (price: number) => {
+        return currency === "EUR" ? `€${Intl.NumberFormat("de").format(price)}` : `$${Intl.NumberFormat("us").format(price)}`;
     };
 
+    useEffect(() => {
+        (async () => {
+            const metalPrices = await getMetalPrices({ metal: metalType, timeframe, });
+            const rates = Object.entries(metalPrices.rates);
+
+            const convertedCurrencies = await getConvertedCurrency({ currency, rates: rates.map(([_, r]) => r.USD) });
+
+            setLocalePrices(
+                rates.map(([date], i) => ({
+                    date: DateTime.fromISO(date).toFormat("LLL dd"),
+                    Price: convertedCurrencies[i].result
+                }))
+            );
+        })();
+    }, [currency, metalType, timeframe]);
+
     return <>
-        <Card className="mt-6">
+        <Card>
             <Text>Current Price</Text>
             <div className="mt-1 flex space-x-4">
-                <Metric>{formatPrice(price)}</Metric>
+                <Metric>{matchCurrencyType(localePrices.at(-1)?.Price ?? 0)}</Metric>
                 <SelectBox 
                     className="w-12"
                     defaultValue={currency} 
                     onValueChange={value => setCurrency(value as CurrencyType)}
                 >
-                    <SelectBoxItem value="sgd" text="SGD"/>
-                    <SelectBoxItem value="usd" text="USD"/>
-                    <SelectBoxItem value="eur" text="EUR"/>
+                    <SelectBoxItem value="SGD" text="SGD"/>
+                    <SelectBoxItem value="USD" text="USD"/>
+                    <SelectBoxItem value="EUR" text="EUR"/>
                 </SelectBox>
             </div>
 
             <AreaChart
-                className="mt-4"
-                data={chartdata}
+                className="mt-8"
+                data={localePrices}
                 index="date"
                 showLegend={false}
-                categories={["Trading Volume"]}
+                categories={["Price"]}
                 colors={["indigo"]}
-                valueFormatter={formatPrice}
+                valueFormatter={matchCurrencyType}
             />
-            <TabList className="flex justify-evenly w-full">
+            <TabList className="flex justify-evenly w-full" onValueChange={value => setTimeframe(value as TradingTimeframe)}>
                 <Tab value="1y" text="1y"/>
                 <Tab value="6m" text="6m"/>
                 <Tab value="1m" text="1m"/>
