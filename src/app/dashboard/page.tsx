0a8f867941bf3fadf215c3913559c88a99a24687
Favian <favouriteteo@gmail.com>
2023-06-05 04:13:41 +0800

    refactor: layout changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,23 @@
+import { Container } from "@/components/Container";
+import { TradingBox } from "@/components/dashboard/TradingBox";
 import { TradingChart } from "@/components/dashboard/TradingChart";
-import { Card, Grid, Subtitle, Title } from "@tremor/react";
+import { Card, Col, Grid, Subtitle, Title } from "@tremor/react";
 
-
-export default function DashboardPage() {   
+export default async function DashboardPage() {   
     return <>
         <main>
-            <div className="container mx-auto">
+            <Container>
                 <Title className="font-bold text-2xl">Dashboard</Title>
-                <Subtitle className="mt-1">View live trading data on various precious metals</Subtitle>
-                <TradingChart />
+                <Subtitle className="mt-1 mb-6">View live trading data on various precious metals.</Subtitle>
+
+                <Grid numColsLg={4} className="gap-6">
+                    <Col numColSpanLg={3}>
+                        <TradingChart />
+                    </Col>
+                    <Col numColSpanLg={1}>
+                        <TradingBox />
+                    </Col>
+                </Grid>
 
                 <Grid numColsMd={2} className="mt-6 gap-6">
                     <Card>
@@ -18,7 +27,7 @@ export default function DashboardPage() {
                         <div className="h-28" />
                     </Card>
                 </Grid>
-            </div>
+            </Container>
         </main>
     </>;
 }
