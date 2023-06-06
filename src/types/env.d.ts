12bb5c3eca45345dbd28bbe4fa0407704b49c0b8
Favian <favouriteteo@gmail.com>
2023-06-06 09:39:19 +0800

    feat: updated env types

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -6,6 +6,7 @@ declare namespace NodeJS {
         readonly NEWSAPI_API_KEY: string;
         readonly ALPHAVANTAGE_API_KEY: string;
         readonly FIREBASE_API_KEY: string;
-        readonly NEXT_PUBLIC_METALPRICE_API_KEY: string;
+        readonly METALPRICE_API_KEY: string;
+        readonly OPENAI_API_KEY: string;
     }
 }
