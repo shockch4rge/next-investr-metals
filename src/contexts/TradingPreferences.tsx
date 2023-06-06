9e8ed72dd4d4f336f4793d2287132154f7120588
Favian <favouriteteo@gmail.com>
2023-06-06 09:40:19 +0800

    refactor: removed 1d timeframe

--- src/contexts/TradingPreferences.tsx
+++ src/contexts/TradingPreferences.tsx
@@ -5,7 +5,7 @@ import { createContext, useContext, useEffect, useState } from "react";
 
 export type MetalType = "XAG" | "XAU" | "XPD" | "XPT";
 export type CurrencyType = "EUR" | "SGD" | "USD";
-export type TradingTimeframe = "1d" | "1m" | "1w" | "1y" | "6m";
+export type TradingTimeframe = "1m" | "1w" | "1y" | "6m";
 
 interface State {
     metalType: MetalType;
@@ -33,7 +33,9 @@ export const TradingPreferencesProvider: React.FC<PropsWithChildren> = ({ childr
     const [metalType, setMetalType] = useState<MetalType>(initialState.metalType);
     const [timeframe, setTimeframe] = useState<TradingTimeframe>(initialState.timeframe);
 
-    return <TradingPreferencesContext.Provider value={{ currency, setCurrency, timeframe, setTimeframe, setMetalType, metalType }}>
+    return <TradingPreferencesContext.Provider 
+        value={{ currency, setCurrency, timeframe, setTimeframe, setMetalType, metalType }}
+    >
         {children}
     </TradingPreferencesContext.Provider>;
 };
